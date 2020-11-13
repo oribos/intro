81d844d6b68fd41ef9bdefefafd55f51d0474f86
denalon <denalon@qq.com>
2020-11-13 10:20:46 +0800

    3

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `云上记事本`,
   },
   plugins: [
     {
@@ -27,8 +27,8 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        short_name: `oribos`,
+        description: `云上记事本`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
---
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -0,0 +1,52 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      Copyright &copy; {new Date().getFullYear()}.     <a class="theme-link" href="http://beian.miit.gov.cn/"> 豫ICP备2020027870号-2</a> All rights reserved. 
+       <br />
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+        <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_w30.png" alt="LekoArts Logo" />
+        {` `}
+        <Link
+          aria-label="Link to the theme's GitHub repository"
+          sx={{ ml: 2 }}
+          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-cara"
+        >
+          Theme
+        </Link>
+        <div sx={{ mx: 1 }}>由</div>
+        {` `}
+        <Link aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en">
+          LekoArts
+        </Link>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer
